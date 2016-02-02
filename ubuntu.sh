ba4ade85a6c3bdac3f99333e1cb49a3354cb8b4d
Rafael Raposo <orapouso@gmail.com>
2016-02-02 19:57:57 -0200

    Updating script
     - Remove oracle java, nuvola player
     - Update nodejs to use official ppa
     - Added steam
     - Update zen shell install

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,51 +1,43 @@
-#oracle-java
-sudo add-apt-repository -y ppa:webupd8team/java
-
-#nuvola player
-sudo apt-add-repository -y ppa:nuvola-player-builders/stable
-
-#terminator
+# terminator
 sudo add-apt-repository -y ppa:gnome-terminator
 
-#node
-sudo add-apt-repository -y ppa:chris-lea/node.js
-
-#numix
+# numix
 sudo add-apt-repository -y ppa:numix/ppa
 
-#sublime text 3
+# sublime text 3
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 
-#atom text editor
+# atom text editor
 sudo add-apt-repository -y ppa:webupd8team/atom
+
+# nodejs - does apt-get update
+#LTS
+#curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+#Stable
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 
 sudo apt-get update
 
-#general
-sudo apt-get install chromium-browser vim vlc easytag nuvolaplayer terminator phatch gdebi
+# general
+sudo apt-get install chromium-browser vim vlc easytag terminator gdebi cifs-util zsh ubuntu-restricted-extras
 
-#dev
-sudo apt-get install mysql-server mysql-workbench meld git gitg git-flow openjdk-7-jdk oracle-java7-installer python-software-properties python python-pip nodejs mongodb-server atom sublime-text-installer
+# dev
+sudo apt-get install mysql-server mysql-workbench meld git gitg git-flow openjdk-8-jre python-software-properties python python-pip nodejs mongodb-server atom sublime-text-installer
 
-#gaming
+# gaming
 sudo apt-get install playonlinux
 
-#theme
+# numix theme
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-icon-theme-shine numix-icon-theme-utouch
 
 wget -P /tmp https://dl.google.com/linux/direct/google-chrome-unstable_current_amd64.deb
 wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget -P /tmp http://media.steampowered.com/client/installer/steam.deb
 
-sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb google-chrome-unstable_current_amd64.deb
+sudo dpkg -i /tmp/google-chrome-stable_current_amd64.deb /tmp/google-chrome-unstable_current_amd64.deb /tmp/steam.deb
 
 sudo pip install virtualenv
 
 #oh-my-zsh shell
-curl -L http://install.ohmyz.sh | sh
-
-#oh-my-fish shell
-curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish
-grep -q '^/usr/bin/fish$' /etc/shells; or echo '/usr/bin/fish' | sudo tee -a /etc/shells
-chsh -s /usr/bin/fish
-
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
