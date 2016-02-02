ace342713633f6a4c4c3e84606e94948a7fdc733
Rafael Raposo <orapouso@gmail.com>
2016-02-02 20:10:44 -0200

    Using nave.sh node env instead of installing node globally
     - Added basher for nave.sh install

--- ubuntu.sh
+++ ubuntu.sh
@@ -14,7 +14,8 @@ sudo add-apt-repository -y ppa:webupd8team/atom
 #LTS
 #curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 #Stable
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+#curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+#using nave.sh
 
 sudo apt-get update
 
@@ -22,7 +23,7 @@ sudo apt-get update
 sudo apt-get install chromium-browser vim vlc easytag terminator gdebi cifs-util zsh ubuntu-restricted-extras
 
 # dev
-sudo apt-get install mysql-server mysql-workbench meld git gitg git-flow openjdk-8-jre python-software-properties python python-pip nodejs mongodb-server atom sublime-text-installer
+sudo apt-get install mysql-server mysql-workbench meld git gitg git-flow openjdk-8-jre python-software-properties python python-pip mongodb-server atom sublime-text-installer
 
 # gaming
 sudo apt-get install playonlinux
@@ -41,3 +42,12 @@ sudo pip install virtualenv
 #oh-my-zsh shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# basher
+git clone https://github.com/basherpm/basher.git ~/.basher
+echo 'export PATH="$HOME/.basher/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(basher init -)"' >> ~/.zshrc
+
+# nave.sh - Virtual Environments for Node
+basher install isaacs/nave
+nave install 5
+sudo nave usemain 5
